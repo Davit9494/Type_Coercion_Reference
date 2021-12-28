7bdaa28c8d42737eb143a7ded877865ac9bc93a5
Davit9494 <davitsahakyan94@mail.ru>
2021-12-28 22:39:19 +0400

    feat: Created t1_numberToString.js

---
+++ t1_numberToString.js
@@ -0,0 +1,7 @@
+"use strict";
+5 + 3; // 8
+5 + NaN; // NaN
+5 + "hello"; // "5hell0"
+5 + a + b + c + d + e + f; // Reff. error
+"hello" + NaN; // "helloNaN"
+"hello" + a + b + c + d + e + f; // Reff Error
