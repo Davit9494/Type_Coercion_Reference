20d296d6b8aa38ca16f6e225aac31a57136ac2e5
Davit9494 <davitsahakyan94@mail.ru>
2021-12-28 22:34:48 +0400

    feat: Created t1_undefined.js file

---
+++ t1_undefined.js
@@ -0,0 +1,7 @@
+"use strict";
+undefined + true; // NaN                 NaN + 1 = NaN
+undefined + false; //NaN                  NaN + 0 = NaN
+undefined + 5; // NaN                     NaN + 5 = NaN
+undefined + NaN; // NaN                   NaN + NaN = NaN
+undefined + "hello"; // "undefinedhello"
+//undefined + a + b + c + d + e + f; // ref error
